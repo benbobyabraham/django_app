3b24375508dfbc4ed16382732a9fe8f18030db6f
benbobyabraham <benbobyabraham@gmail.com>
2022-07-21 07:16:58 +0530

    simple-crud-app-reference

---
+++ listings/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=100)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
---
+++ real_estate/urls.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from listings.views import (
+    listing_list,
+    listing_retrieve,
+    listing_create,
+    listing_update,
+    listing_delete
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', listing_list),
+    path('listings/<pk>/', listing_retrieve),
+    path('listings/<pk>/edit/', listing_update),
+    path('listings/<pk>/delete/', listing_delete),
+    path('add-listing/', listing_create),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                            document_root=settings.MEDIA_ROOT)
